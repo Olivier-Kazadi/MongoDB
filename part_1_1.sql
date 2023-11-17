165c08718dfbb2e4be19625024cc06c0eff669f7
Olivier-Kazadi <63251013+Olivier-Kazadi@users.noreply.github.com>
2023-11-17 09:58:44 +0100

    Add files via upload

--- part_1_1.sql
+++ part_1_1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_table (
-    user_id smallint PRIMARY KEY,
+    user_id serial PRIMARY KEY,
     firstname character varying,
     lastname character varying,
     email character varying UNIQUE,
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS user_table (
 );
 
 CREATE TABLE IF NOT EXISTS user_email_verification (
-    uev_id smallint PRIMARY KEY,
+    uev_id serial PRIMARY KEY,
     user_id smallint,
     verified_at DATE
 );
 
 CREATE TABLE IF NOT EXISTS session_table (
-    session_id smallint PRIMARY KEY,
+    session_id serial PRIMARY KEY,
     user_id smallint,
     connected_at DATE
 );
